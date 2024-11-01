ef56853f037179ff5ecd45c4f196122c17272c51
jonathancode01 <icontatojonathan@gmail.com>
2024-11-01 13:23:00 -0300

    a

--- app.py
+++ app.py
@@ -3,18 +3,26 @@ from kivy.app import App
 from kivy.uix.button import Button
 from kivy.uix.label import Label
 from kivy.uix.boxlayout import BoxLayout
+from kivy.core.window import Window
 
 # Definir a classe principal do aplicativo
 class MeuApp(App):
     def build(self):
+        # Definir a cor de fundo da janela
+        Window.clearcolor = (0.9, 0.9, 0.9, 1)  # Cor de fundo cinza claro
+
         # Criar um layout vertical
-        layout = BoxLayout(orientation='vertical')
+        layout = BoxLayout(orientation='vertical', padding=20, spacing=10)
 
-        # Criar um título como Label
-        titulo = Label(text="Geinf", font_size=40, size_hint=(1, 0.3))
+        # Criar um título 
+        titulo = Label(
+            text="Geinf - Gerencia de infraestrutura", 
+            font_size=40, 
+            size_hint=(1, 0.3),
+            color=(0.1, 0.4, 0.6, 1)  # Cor do texto azul escuro
+        )
 
-        # Criar um botão
-        botao = Button(text="Clique Aqui", font_size=30)
+
 
         # Conectar o botão à função que será executada quando o botão for clicado
         botao.bind(on_press=self.mostrar_mensagem)
